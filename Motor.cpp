faa89257a4e283e46d39a44c94dd5af139aa9b67
mr.Shu <mrshux@gmail.com>
2011-05-24 18:03:11 +0000

    first commit

---
+++ Motor.cpp
@@ -0,0 +1,58 @@
+/*
+ * Motor.cpp
+ * This file is part of Motor
+ *
+ * Copyright (C) 2010 - mr.Shu
+ *
+ * Motor is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Motor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Motor; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, 
+ * Boston, MA  02110-1301  USA
+ */
+ 
+#include "WProgram.h"
+#include "Motor.h"
+
+Motor::Motor(int dir, int pwm)
+{
+	pinMode(dir,OUTPUT);
+	pinMode(pwm,OUTPUT);
+	_dir = dir;
+	_pwm = pwm;
+}
+
+void Motor::go(int speed)
+{
+	int type = HIGH;
+	//if speed is negative, rotate motor to another direction
+	if(speed < 0){
+		type = LOW;
+	}
+	
+	//use speed values only from range <0,255>
+	speed = abs(speed % 256);
+	
+	//write direction
+	digitalWrite(_dir, type);
+	//write speed
+	analogWrite(_pwm, speed);
+}
+
+void Motor::stop()
+{
+	digitalWrite(_dir, HIGH);
+	//set speed 0 (stop the motor)
+	analogWrite(_pwm,0);
+}
+
+
---
+++ README.md
@@ -0,0 +1,46 @@
+
+Motor
+=====
+
+Motor is a simple Arduino library for [Ardumoto][] motor shield.
+
+Installation
+------------
+
+Download the Motor library and put it into a folder in your
+`sketchbook/libraries` directory.
+
+In Linux like environment you can do something like this
+
+
+	~/sketchbook/libraries/ $ git clone git://github.com/mrshu/Motor.git
+
+
+Usage
+-----
+
+You can just include Motor library in your Arduino sketch by putting 
+
+
+	#include <Motor.h>
+
+Then you can define a new motor 
+	
+	Motor motorA = Motor(13, 11);
+
+where `13` is  the direction pin and `11` is the PWM pin on Ardumoto.
+
+Next thing you might want to do is to move the motor. This can be done this way
+
+	motorA.go(200);
+
+where `200` is the speed. Note that the maximal value of speed is 255. You might
+also want to stop the motor:
+
+	motorA.stop();
+
+
+(c) 2010 mr.Shu
+
+[Ardumoto]: http://www.sparkfun.com/products/9571
+
