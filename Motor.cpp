d6d9e0e93b137c0f97c0f1e2699da3217de518aa
mr.Shu <mr@shu.io>
2013-05-22 14:59:07 +0200

    stuff changed

--- Motor.cpp
+++ Motor.cpp
@@ -2,7 +2,7 @@
  * Motor.cpp
  * This file is part of Motor
  *
- * Copyright (C) 2010 - XG
+ * Copyright (C) 2010 - 2013, mrShu
  *
  * Motor is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,10 +16,10 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Motor; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, 
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
  * Boston, MA  02110-1301  USA
  */
- 
+
 #include "WProgram.h"
 #include "Motor.h"
 
@@ -38,10 +38,10 @@ void Motor::go(int speed)
 	if(speed < 0){
 		type = LOW;
 	}
-	
+
 	//use speed values only from range <0,255>
 	speed = abs(speed % 256);
-	
+
 	//write direction
 	digitalWrite(_dir, type);
 	//write speed
--- Motor.h
+++ Motor.h
@@ -1,8 +1,8 @@
 /*
- * Motor.h - Library for controling DC motors connected to Ardumoto 
+ * Motor.h - Library for controling DC motors connected to Ardumoto
  * This file is part of Motor
  *
- * Copyright (C) 2010 - XG
+ * Copyright (C) 2010 - 2013, mrShu
  *
  * Motor is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,11 +16,11 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Motor; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, 
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
  * Boston, MA  02110-1301  USA
  */
- 
- 
+
+
 #ifndef Motor_H
 #define Motor_H
 
